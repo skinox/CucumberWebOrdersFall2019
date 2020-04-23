bc1f5cdf5b9468a76bb98519a2cdc1199058f01a
skinox <jumaev@Aidars-MBP.verizon.net>
2020-04-22 21:19:26 -0400

    second commit

---
+++ src/test/java/com/weborders/utilities/Driver.java
@@ -0,0 +1,61 @@
+package com.weborders.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static java.util.logging.Level.OFF;
+
+public class Driver {
+
+    //same for everyone
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
+
+    //so no one can create object of Driver class
+    //everyone should call static getter method instead
+    private Driver() {
+
+    }
+
+    public synchronized static WebDriver getDriver() {
+        //if webdriver object doesn't exist
+        //create it
+        if (driverPool.get() == null) {
+            //specify browser type in configuration.properties file
+            String browser = ConfigurationReader.getProperty("browser").toLowerCase();
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--start-maximized");
+                    java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(OFF);
+                    System.setProperty("webdriver.chrome.silentOutput", "true"); //ignore chrome version logs
+                    driverPool.set(new ChromeDriver(chromeOptions));
+                    break;
+                case "chromeheadless":
+                    //to run chrome without interface (headless mode)
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.setHeadless(true);
+                    driverPool.set(new ChromeDriver(options));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    break;
+                default:
+                    throw new RuntimeException("Wrong browser name!");
+            }
+        }
+        return driverPool.get();
+    }
+
+    public static void closeDriver() {
+        if (driverPool != null) {
+            driverPool.get().quit();
+            driverPool.remove();
+        }
+    }
+}
